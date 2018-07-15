34cfae36d8567b3042c35f1146c89efbb88fac0e
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-07-15 07:07:45 +0100

    provisioner for zf3

--- upgradeScripts/zf3.sh
+++ upgradeScripts/zf3.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
+IFS=
 set -e
+SETUP=$1
+#warnign dangerous ARG used for development ease only
+if [ $SETUP='CLEAN' ]
+ then
+    `rm -rf ../zf3`
+fi
 
 mkdir ../zf3
 cd ../zf3
@@ -34,10 +40,26 @@ declare -a repos=(
 #do
 #	git clone $i
 #done
+STARTCOMMENT="# start olcs_v2"
+ENDCOMMENT="# end olcs_v2"
+VAGRANTFILE="../infrastructure/Vagrantfile"
+vm=`sed -n "/$STARTCOMMENT/,/$ENDCOMMENT/p" $VAGRANTFILE`
+vm=$(echo "$vm" | sed "s/olcs_v2/olcs_v3/g")
+vm=$(echo "$vm" | sed "s/'olcs-v2'/'olcs-v3'/g")
 
-vm=`sed -n '/# start olcs_v2/,/# end olcs_v2/p' ../infrastructure/Vagrantfile`
+COMMENT=`grep -n "$ENDCOMMENT" $VAGRANTFILE | cut -d: -f 1`
+COMMENT=$(($COMMENT+1))
 
-vm=$(echo "$vm" | sed "s/\'olcs_v2\'/olcs_v3/g")
+ZF3Vagrant=$VAGRANTFILE"-ZF3"
+echo $vm > $ZF3Vagrant
 
-echo $vm
+# server_vagrantfile = File.expand_path('../vagrant/Vagrantfile.server', __FILE__)
+# eval File.read(server_vagrantfile) if File.exists?(server_vagrantfile)
+echo "updating Vagrant file at line $COMMENT"
+INCLUDE="NR=="$COMMENT
+INCLUDE2="NR=="$(($COMMENT+1))
+echo $INCLUDE2
+echo "`awk $INCLUDE'{print "# zf3_vagrant = File.expand_path('$ZF3Vagrant',__FILE__)"}1' $VAGRANTFILE`" > $VAGRANTFILE
+echo "`awk $INCLUDE2'{print "# eval File.read(zf3_vagrant)  if File.exists?(zf3_vagrant)"}1' $VAGRANTFILE`" > $VAGRANTFILE
+
 
